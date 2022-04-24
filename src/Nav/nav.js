9de3131c2be062df4c0bac5b66b0fa927aab2f30
fattie <mpaulperr@gmail.com>
2022-04-23 22:50:22 -0400

    4/23 update

--- src/Nav/nav.js
+++ src/Nav/nav.js
@@ -1,14 +1,37 @@
-import LookUp from './LookUp/lookup';
-import Saved from './Saved/saved';
-
 import './nav.css'
-import React from 'react';
+import React, { useState } from 'react';
 
 function Nav() {
+
+    const BASE = ;
+    const CLIENT_ID = 'oedbd2ioqzs04nc43hvmkbyhjyy3ri';
+    const SECRET = 'f30fon3ca70b8gxftt96xwxwb1c5g4';
+
+    const [query, setQuery] = useState('');
+    const [weather, setWeather] = useState({});
+  
+    const search = evt => {
+      if (evt.key === "Enter") {
+        fetch(`${base}weather?q=${query}&units=metric&APPID=${key}`)
+          .then(res => res.json())
+          .then(result => {
+            setWeather(result);
+            setQuery('');
+            console.log(result);
+          });
+      }
+    }
+
     return (
-        <div className="nav">
-            <LookUp />
-            <Saved />
+        <div className="lookup">
+            <input 
+                type="text"
+                className="search-bar"
+                placeholder="Search for a game..."
+                onChange={e => setQuery(e.target.value)}
+                value={query}
+                onKeyPress={search}
+            />
         </div>
     );
 }
